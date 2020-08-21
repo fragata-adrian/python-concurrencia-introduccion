5a1f4e237e4d4b1d6f1ad84a2b2bc3fd989a770e
fragata-adrian <fragataadrian99@gmail.com>
2020-08-21 20:25:54 -0300

    notas clase 2

--- contadorConcurrente.py
+++ contadorConcurrente.py
@@ -5,12 +5,38 @@ MAX_COUNT = 1000000
 
 counter = 0
 
+mutex = threading.Lock()
+
+'''
+-Con semaforos contador-
+
+semaforo = threading.Semaphore(2)
+
+semaforo.acquire()
+semaforo.acquire()
+...
+semaforo.release()
+'''
 
 def cuenta():
     global counter
 
     for i in range(int(MAX_COUNT/THREADS)):
+        #Opcion 1
+        mutex.acquire()
         counter += 1
+        mutex.release()
+
+        #Opcion 2
+        #mutex.acquire()
+        #try:
+        #    counter += 1
+        #finally:
+        #    mutex.release()
+        
+        #Opcion 3
+        #with mutex:
+        #    counter += 1
 
 
 
